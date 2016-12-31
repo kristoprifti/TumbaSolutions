c10be3c61e4860d7e0e1fe1232e353ce57fbfc85
Kristo Prifti <kristoprifti93@gmail.com>
2016-12-31 22:50:39 +0200

    Initial commit

---
+++ app/src/main/java/me/kristoprifti/android/tumbasolutions/MainActivity.java
@@ -0,0 +1,158 @@
+package me.kristoprifti.android.tumbasolutions;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private static final int TAKE_PICTURE = 100;
+    private static final int MY_PERMISSIONS_REQUEST = 1;
+
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        int hasReadExternalStoragePermission = ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE);
+        int hasWriteExternalStoragePermission = ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE);
+        int hasCameraPermissions = ContextCompat.checkSelfPermission(this, CAMERA);
+
+        if(hasCameraPermissions != PackageManager.PERMISSION_GRANTED &&
+                hasReadExternalStoragePermission != PackageManager.PERMISSION_GRANTED &&
+                hasWriteExternalStoragePermission != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "onCreate: requesting permissions");
+            ActivityCompat.requestPermissions(this, new String[] {CAMERA, READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST);
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Here, thisActivity is the current activity
+                if(ContextCompat.checkSelfPermission(MainActivity.this, CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                        ContextCompat.checkSelfPermission(MainActivity.this, READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                        ContextCompat.checkSelfPermission(MainActivity.this, WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    takePhoto();
+                } else {
+                    Snackbar.make(view, "This app can't take pictures unless you...", Snackbar.LENGTH_INDEFINITE)
+                            .setAction("Grant Access", new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            Log.d(TAG, "Snackbar onClick: starts");
+                                            if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, CAMERA) ||
+                                                    ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, READ_EXTERNAL_STORAGE) ||
+                                                    ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, WRITE_EXTERNAL_STORAGE)) {
+                                                Log.d(TAG, "Snackbar onClick: calling requestPermissions");
+                                                ActivityCompat.requestPermissions(MainActivity.this, new String[] {CAMERA, READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE},
+                                                        MY_PERMISSIONS_REQUEST);
+                                            } else {
+                                                // The user has permanently denied the permission, so take them to the settings
+                                                Log.d(TAG, "Snackbar onClick: launching settings");
+                                                Intent intent = new Intent();
+                                                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                                Uri uri = Uri.fromParts("package", MainActivity.this.getPackageName(), null);
+                                                Log.d(TAG, "Snackbar onClick: Intent Uri is " + uri.toString());
+                                                intent.setData(uri);
+                                                MainActivity.this.startActivity(intent);
+                                            }
+                                            Log.d(TAG, "Snackbar onClick: ends");
+                                        }
+                            })
+                            .show();
+                }
+                Log.d(TAG, "fab onClick: ends");
+            }
+        });
+        Log.d(TAG, "onCreate: ends");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PICTURE:
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri selectedImage = imageUri;
+                    getContentResolver().notifyChange(selectedImage, null);
+                    ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                    ContentResolver cr = getContentResolver();
+                    Bitmap bitmap;
+                    try {
+                        bitmap = android.provider.MediaStore.Images.Media
+                                .getBitmap(cr, selectedImage);
+                        imageView.setImageBitmap(bitmap);
+                        Toast.makeText(this, selectedImage.toString(),
+                                Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
+                                .show();
+                        Log.e("Camera", e.toString());
+                    }
+                }
+        }
+    }
+}
